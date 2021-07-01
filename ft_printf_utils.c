b9e2206b8073a9cdee22fc456593f4eacfcc3d81
Erchegyi David <edavid@3-F-5.42heilbronn.de>
2021-07-01 17:01:13 +0200

    ft_printf_utils.c norm check

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: edavid <edavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 15:43:59 by edavid            #+#    #+#             */
-/*   Updated: 2021/07/01 16:08:56 by edavid           ###   ########.fr       */
+/*   Updated: 2021/07/01 17:00:55 by edavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,36 +15,44 @@
 #include "libft/libft.h"
 #include "ft_conversions.h"
 
-char	*malloc_conv_spec(char *format, int *format_index) // shave 10 lines
+static char	*alloc_null_str(void)
+{
+	char	*str;
+
+	str = malloc(1);
+	if (!str)
+		return ((char *)0);
+	*str = '\0';
+	return (str);
+}
+
+static void	handle_precision(int *index, char *format)
+{
+	(*index)++;
+	if (format[*index] == '*')
+		(*index)++;
+	else
+		while (ft_isdigit(format[*index]))
+			(*index)++;
+}
+
+char	*malloc_conv_spec(char *format, int *format_index)
 {
 	int		index;
 	int		format_len;
-	char	*conversion_specifier; 
+	char	*conversion_specifier;
 
 	if (!*format)
-	{
-		conversion_specifier = malloc(1);
-		*conversion_specifier = '\0';
-		return (conversion_specifier);
-	}
+		return (alloc_null_str());
 	index = 0;
 	while (format[index] == '-' || format[index] == '0')
 		index++;
 	while (ft_isdigit(format[index]))
 		index++;
 	if (format[index] == '*')
-	{
 		index++;
-	}
 	if (format[index] == '.')
-	{
-		index++;
-		if (format[index] == '*')
-			index++;
-		else
-			while (ft_isdigit(format[index]))
-				index++;
-	}
+		handle_precision(&index, format);
 	format_len = index + 1;
 	conversion_specifier = malloc(format_len + 1);
 	index = -1;
@@ -55,7 +63,7 @@ char	*malloc_conv_spec(char *format, int *format_index) // shave 10 lines
 	return (conversion_specifier);
 }
 
-int		is_valid_conv_spec(char *conv_spec) // looks good
+int	is_valid_conv_spec(char *conv_spec)
 {
 	if (*conv_spec == '%')
 		return (1);
@@ -74,15 +82,15 @@ int		is_valid_conv_spec(char *conv_spec) // looks good
 			while (ft_isdigit(*conv_spec))
 				conv_spec++;
 	}
-	if (*conv_spec == '\0' || (*conv_spec != 'c' && *conv_spec != 's' &&
-	*conv_spec != 'p' && *conv_spec != 'd' && *conv_spec != 'i' &&
-	*conv_spec != 'u' && *conv_spec != 'x' && *conv_spec != 'X' &&
-	*conv_spec != '%'))
+	if (*conv_spec == '\0' || (*conv_spec != 'c' && *conv_spec != 's'
+			&& *conv_spec != 'p' && *conv_spec != 'd' && *conv_spec != 'i'
+			&& *conv_spec != 'u' && *conv_spec != 'x' && *conv_spec != 'X'
+			&& *conv_spec != '%'))
 		return (0);
 	return (1);
 }
 
-int		handle_conversion_spec(char *conv_spec, va_list ap) // looks good
+int	handle_conversion_spec(char *conv_spec, va_list ap)
 {
 	int		*flags;
 	int		conversion_index;
